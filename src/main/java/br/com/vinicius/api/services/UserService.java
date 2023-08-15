eacbccec342e20760bd7f9fef53dac94e67367de
viniciuscaine <viniciuscainee@gmail.com>
2023-08-15 12:04:20 -0300

    endpoint findAll para user

--- src/main/java/br/com/vinicius/api/services/UserService.java
+++ src/main/java/br/com/vinicius/api/services/UserService.java
@@ -2,6 +2,9 @@ package br.com.vinicius.api.services;
 
 import br.com.vinicius.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
