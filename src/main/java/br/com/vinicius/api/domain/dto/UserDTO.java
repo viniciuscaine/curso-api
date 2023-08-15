eefbbec7306fb38aef420926754ed7f16667a0c7
viniciuscaine <viniciuscainee@gmail.com>
2023-08-15 11:46:15 -0300

    classe user dto

---
+++ src/main/java/br/com/vinicius/api/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package br.com.vinicius.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
